0ddd1fa735225287e812d4f5355b0ca49d8919a4
koa-s <143123833+koa-s@users.noreply.github.com>
2024-01-16 11:19:14 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -6,10 +6,39 @@ public class GoogleBillboard {
 		System.out.println("Not the right string, tests will fail!");
 	}
 	
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+boolean isFound;
+int count;
 
-	//Finish this function
-	public boolean isPrime(){
-	}
+public void setup()  
+{            
+  isFound = false;
+  count = 2;
+  while (isFound == false)
+  {
+    String digits = e.substring(count, count+10);
+    double dNum = Double.parseDouble(digits);
+    if (isPrime(dNum)==true)
+    {
+      isFound = true;
+      System.out.println(dNum);
+    }
+    count++;
+  }
+}  
 
-}
-
+public boolean isPrime(double num)  
+{   
+  if (num<=1)
+  {
+    return false;
+  }
+  for (int i = 2; i<=Math.sqrt(num); i++)
+  {
+    if (num%i==0)
+    {
+      return false;
+    }
+  }
+  return true;
+} 
